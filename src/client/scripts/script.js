ca86299fb3be1a780dc0385a9eff0d9860a0f280
Qi Mu <skycloud112@gmail.com>
2016-10-22 19:09:59 -0400

    add browser history

--- src/client/scripts/script.js
+++ src/client/scripts/script.js
@@ -1,15 +1,18 @@
 // Libs
 import React from 'react';
 import { render } from 'react-dom';
+import { Router, Route, browserHistory } from 'react-router';
 
 // CSS
 import '../../app/css/style.css';
 
 // Components
 import App from '../../app/App';
+import Home from '../../app/components/Home';
 
 // Render
-render(
-  <App />,
-  document.getElementById('root')
-);
+render((
+  <Router history={browserHistory}>
+    <Route path="/" component={Home} />
+  </Router>
+),document.getElementById('root'));
