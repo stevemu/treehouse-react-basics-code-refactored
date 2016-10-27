c908c2201380691eeabbe7574d77ea9aa7f3f27f
stevemu <steve.mu.dev@gmail.com>
2016-10-27 18:13:08 -0400

    new content

--- src/app/components/courses/CourseContainer.js
+++ src/app/components/courses/CourseContainer.js
@@ -1,25 +1,22 @@
 import React, { Component } from 'react';
-
 import Course from './Course';
-import CourseList from '../../data/courses';
 
-class JavaScript extends Component {
+class CourseContainer extends Component {
   render() {
-    let courseList = CourseList.JS;
-    let courses = courseList.map((course) => {
+    let courses = this.props.route.data.map((course) => {
       return <Course title={course.title}
                      desc={course.description}
                      img={course.img_src}
                      key={course.id} />
-    }); 
+    });
     return (
       <div>
         <ul>
-          {courses}    
+          {courses}
         </ul>
       </div>
     );
   }
 }
 
-export default JavaScript;
+export default CourseContainer;
--- src/app/route.js
+++ src/app/route.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Router, Route, browserHistory } from 'react-router';
+import { Router, Route, browserHistory, IndexRedirect, IndexRoute } from 'react-router';
 
 // Components
 import App from './App';
@@ -9,16 +9,33 @@ import Featured from './components/Featured';
 import Teachers from './components/Teachers';
 import About from './components/About';
 
+// Courses components
+// import CSS from './components/courses/CSS';
+// import HTML from './components/courses/HTML';
+import CourseContainer from './components/courses/CourseContainer';
+
+import NotFound from './components/NotFound';
+import CourseList from './data/courses';
+
 const route = (
   <Router history={browserHistory}>
     <Route component={App}>
       <Route path="/" component={Home} />
       <Route path="/about" component={About} />
-      <Route path="/courses" component={Courses} />
+      <Route path="/courses" component={Courses} >
+        <IndexRedirect to="/courses/html" />
+        <Route path="/courses/css" component={CourseContainer} data={CourseList.CSS}/>
+        <Route path="/courses/html" component={CourseContainer} data={CourseList.HTML} />
+        <Route path="/courses/javascript" component={CourseContainer} data={CourseList.JS}/>
+      </Route>
       <Route path="/featured" component={Featured} />
       <Route path="/teachers" component={Teachers} />
+      <Route path="*" component={NotFound}/>
     </Route>
   </Router>
 );
+
+
+//  <IndexRoute component={CSS} />
 
 export default route;
